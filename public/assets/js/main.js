b7cbc7f17d482c2470227ba68c6a8bd0735dd7a4
kalimbak <kevin.kalimba54@gmail.com>
2022-02-24 14:56:05 +0200

    new comitt

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -94,41 +94,6 @@ modalCloses.forEach((modalClose) => {
     })
 })
 
-// To be Reviewed
-// servicesModals.forEach((servicesModal) => {
-//     servicesModal.addEventListener('click', () => {
-//         if (servicesModalContent.clicked === true) {
-//             alert('You are Clicking Me')
-//         }        
-//         else {
-//             modalViews.forEach((modalView) => {
-//                 modalView.classList.remove('active-modal')
-//             })
-//         }
-//     })
-// })
-
-// Updated Swiper JS 
-
-/* PORTFOLIO SWIPER  */
-
-let swiper = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: false, //Allos Us To Loop Through Pagination
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
-
-/* TESTIMONIAL */
-
 /* Form Control */
 const contactForm = document.getElementById('contactForm')  //.submit()
 
@@ -176,32 +141,9 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp);
 
 
-/* DARK LIGHT THEME */ 
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
-
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme');
 const selectedIcon = localStorage.getItem('selected-icon');
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
-}
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
-})
